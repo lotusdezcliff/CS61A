c4f75319552fffdf247f13778e001b81f230eab8
aaron <boddy.jo.bj@gmail.com>
2025-06-11 19:16:20 +0800

    Finished disc4 count_k

--- Spring 2022/disc/disc4.py
+++ Spring 2022/disc/disc4.py
@@ -11,3 +11,23 @@ def count_stair_ways(n):
         return 0
     else:
         return count_stair_ways(n - 1) + count_stair_ways(n - 2)
+
+def count_k(n, k):
+    """ Counts the number of paths up a flight of n stairs
+    when taking up to and including k steps at a time.
+    >>> count_k(3, 3) # 3, 2 + 1, 1 + 2, 1 + 1 + 1
+    4
+    >>> count_k(4, 4)
+    8
+    >>> count_k(10, 3)
+    274
+    >>> count_k(300, 1) # Only one step at a time
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    else:
+        return sum(count_k(n - m, k) for m in range(1, k + 1))
