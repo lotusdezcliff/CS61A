050282eba619b891215c80ec021ab85b2bfb8f62
aaron <boddy.jo.bj@gmail.com>
2025-06-12 04:01:22 +0800

    Finished disc4 max_product, finised disc4.py

--- Spring 2022/disc/disc4.py
+++ Spring 2022/disc/disc4.py
@@ -44,6 +44,8 @@ def count_k(n, k):
 # >>> a[3][0]
 # 2
 
+# Q4: Even weighted
+# Write a function that takes a list s and returns a new list that keeps only the even-indexed elements of s and multiplies them by their corresponding index.
 def even_weighted(s):
     """
     >>> x = [1, 2, 3, 4, 5, 6]
@@ -51,3 +53,29 @@ def even_weighted(s):
     [0, 6, 20]
     """
     return [s[x] * x for x in range(len(s)) if x % 2 == 0]
+
+# Q5: Max Product
+# Write a function that takes in a list and returns the maximum product that can be formed using nonconsecutive elements of the list. The input list will contain only numbers greater than or equal to 1.
+def max_product(s):
+    """Return the maximum product that can be formed using
+    non-consecutive elements of s.
+    >>> max_product([10,3,1,9,2]) # 10 * 9
+    90
+    >>> max_product([5,10,5,10,5]) # 5 * 5 * 5
+    125
+    >>> max_product([])
+    1
+    """
+    # empty list case
+    if not s:
+        return 1
+    # one number list case
+    elif len(s) == 1:
+        return s[0]
+    # Choose the max path
+    else:
+        # Take: choose the current element and skip the next one
+        take = s[0] * max_product(s[2:])
+        # Skip: skip the current element and choose the take the next one or keep skip the next one
+        skip = max_product(s[1:])
+        return max(take, skip)
