a8da5b8706f9d34a368a0fddb94aa3eb76296b7d
aaron <boddy.jo.bj@gmail.com>
2025-06-13 14:36:42 +0800

    Done disc5 Q1: Map, Filter, Reduce

--- Spring 2022/disc/disc5.py
+++ Spring 2022/disc/disc5.py
@@ -5,3 +5,36 @@ def my_map(fn, seq):
     [1, 4, 9]
     """
     "*** YOUR CODE HERE ***"
+    return [fn(x) for x in seq]
+
+def my_filter(pred, seq):
+    """Keeps elements in seq only if they satisfy pred.
+    >>> my_filter(lambda x: x % 2 == 0, [1, 2, 3, 4])  # new list has only even-valued elements
+    [2, 4]
+    """
+    "*** YOUR CODE HERE ***"
+    return [x for x in seq if pred(x)]
+
+def my_reduce(combiner, seq):
+    """Combines elements in seq using combiner.
+    seq will have at least one element.
+    >>> my_reduce(lambda x, y: x + y, [1, 2, 3, 4])  # 1 + 2 + 3 + 4
+    10
+    >>> my_reduce(lambda x, y: x * y, [1, 2, 3, 4])  # 1 * 2 * 3 * 4
+    24
+    >>> my_reduce(lambda x, y: x * y, [4])
+    4
+    >>> my_reduce(lambda x, y: x + 2 * y, [1, 2, 3]) # (1 + 2 * 2) + 2 * 3
+    11
+    """
+    "*** YOUR CODE HERE ***"
+    if len(seq) == 1:
+        return seq[0]
+    else:
+        while len(seq) >= 2:
+            result = combiner(seq[0], seq[1])
+            seq[0] = result
+            seq.pop(1)
+        return seq[0]
+
+
