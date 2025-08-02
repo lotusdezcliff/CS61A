c97b9074742352860696ce88fad68d3a35b01612
Aaron LOI <boddy.jo.bj@gmail.com>
2025-08-02 16:35:44 +0800

    primes_gen

--- Spring 2022/disc/disc7.py
+++ Spring 2022/disc/disc7.py
@@ -104,5 +104,36 @@ def hailstone(n):
         n =  (lambda x: x // 2 if x % 2 == 0 else 3 * x + 1)(n)
     while True:
         yield 1
-    
+
+# Q6: Primes Generator
+def is_prime(n):
+    """Returns True if n is a prime number and False otherwise.
+    >>> is_prime(2)
+    True
+    >>> is_prime(16)
+    False
+    >>> is_prime(521)
+    True
+    """
+    def helper(i):
+        if i > (n ** 0.5): # Could replace with i == n
+            return True
+        elif n % i == 0:
+            return False
+        return helper(i + 1)
+    return helper(2)
+
+def primes_gen(n):
+    """Generates primes in decreasing order.
+    >>> pg = primes_gen(7)
+    >>> list(pg)
+    [7, 5, 3, 2]
+    """
+    if n < 2:
+        return 
+    if is_prime(n):
+        yield n
+    yield from primes_gen(n - 1)
+
+
     
