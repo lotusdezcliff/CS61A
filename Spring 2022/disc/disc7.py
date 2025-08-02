601dfdfd572889a58bc0e5b7b73dd53a3044a8fb
Aaron LOI <boddy.jo.bj@gmail.com>
2025-08-02 16:00:06 +0800

    filter_iter

--- Spring 2022/disc/disc7.py
+++ Spring 2022/disc/disc7.py
@@ -65,3 +65,26 @@ def remove_all(link, value):
 >>> next(i)
 StopIteration
 '''
+
+# Q4: Filter-Iter
+def filter_iter(iterable, f):
+    """
+    >>> is_even = lambda x: x % 2 == 0
+    >>> list(filter_iter(range(5), is_even)) # a list of the values yielded from the call to filter_iter
+    [0, 2, 4]
+    >>> all_odd = (2*y-1 for y in range(5))
+    >>> list(filter_iter(all_odd, is_even))
+    []
+    >>> naturals = (n for n in range(1, 100))
+    >>> s = filter_iter(naturals, is_even)
+    >>> next(s)
+    2
+    >>> next(s)
+    4
+    """
+    "*** YOUR CODE HERE ***"
+    for i in iterable:
+        if f(i):
+            yield i
+        else:
+            yield 
